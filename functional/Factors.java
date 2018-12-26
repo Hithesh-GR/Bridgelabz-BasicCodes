c97a776ce3b6555ade6ed4c85e45ab43e65356b9
MasterpieceRoy <abhi.roy15@gmail.com>
2018-12-26 18:25:13 +0530

    Second Commit

--- functional/Factors.java
+++ functional/Factors.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.functional;
+import com.bridgelabz.utility.*;
+public class Factors 
+{
 
-public class Factors {
-
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter the number");
+		int prime=util.getInt();
+		util.primeFactor(prime);
 	}
 
 }
