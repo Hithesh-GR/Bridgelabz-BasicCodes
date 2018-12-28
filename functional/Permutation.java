34042aaa1266ce30a885fc5370e41be08f1cd82c
MasterpieceRoy <abhi.roy15@gmail.com>
2018-12-28 11:53:58 +0530

    8th Commit

---
+++ functional/Permutation.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.functional;
+public class Permutation 
+{
+		private static void swap(char[] ch, int i, int j)
+		{
+			char temp = ch[i];
+			ch[i] = ch[j];
+			ch[j] = temp;
+		}
+		private static void permutations(char[] ch, int currentIndex)
+		{
+			if (currentIndex == ch.length - 1) 
+			{
+				System.out.println(String.valueOf(ch));
+			}
+			for (int i = currentIndex; i < ch.length; i++)
+			{
+				swap(ch, currentIndex, i);
+				permutations(ch, currentIndex + 1);
+			}
+		}
+		public static void main(String[] args)
+		{
+			String s = "abc";
+			permutations(s.toCharArray(), 0);
+		}
+}
+	
